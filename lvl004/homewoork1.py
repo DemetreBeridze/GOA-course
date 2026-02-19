9f174948718088a0705aabb4b5a11c72d9cddbed
DemetreBeridze <beridzedemetre21@gmail.com>
2026-02-19 23:34:29 +0400

    commit

---
+++ lvl004/homewoork1.py
@@ -0,0 +1,21 @@
+#davaleba 7
+
+city = input("enter your  city: ")
+District = input("enter your district: ")
+Building = input("enter your building: ")
+Floor = input("enter your floor: ")
+print(f"I live in {city}, I live in {District} district, in {Building} building and on {Floor} floor")
+
+#davaleba 8
+
+age = input("enter your age:")
+print(f"I am {age} years old")
+
+
+#davaleba 9
+
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+
+print(f"{num1}+{num2}= {int(num1) + int(num2)} ")
+
---
+++ lvl006/claswork.py
@@ -0,0 +1,28 @@
+
+num1 = int(input("write first number: " ))
+num2 = int(input("write second number: " ))
+Addition = num1+ num2
+Substraction = num1 -num2
+Multiplication = num1 * num2
+Division =num1 / num2
+
+print (Addition)
+print (Substraction)
+print (Multiplication)
+print (Division)
+
+
+
+print(bool(int(True) - int(False) + int(False))) 
+#es kodi gamoitans "True"-s radgan int(Trua) matematikurad udris erts xolo int(False) matematikurad udris nuls
+
+#data conversations
+#int - mteli ricxvi
+#floar - atwiladi
+#string - teqsti
+#blood - Ttue/False
+
+
+
+
+

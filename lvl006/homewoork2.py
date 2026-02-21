9d81d255e99c6a174d1ab69c912acae5c39d5c22
DemetreBeridze <beridzedemetre21@gmail.com>
2026-02-21 21:37:34 +0400

    commit

---
+++ lvl006/homewoork2.py
@@ -0,0 +1,29 @@
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+num3=int(input("enter third number: "))
+num4=int(input("enter fourth number: "))
+num5=int(input("enter fifth number: "))
+
+num6 = num1+num2+num3+num4+num5
+Arithmetic_mean= num6/5
+
+print(Arithmetic_mean)
+
+
+
+
+
+celsius=int(input("enter celsius: "))
+farengate= celsius*1.8+32
+print(farengate)
+
+
+
+
+farengate1=float(input("enter farengate: "))
+celsius1 = farengate1-32 / 1.8
+print(celsius1)
+
+
+
+
